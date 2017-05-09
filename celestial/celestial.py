d6bdf3fad66dd4f56705feca4668133e3f605b57
tatsu <xndr42@gmail.com>
2017-05-09 11:08:38 -0700

    Fixed Boss Tracking
    Sometimes we would move boss index early, re-enter loop, and then move
    it again. Added a short sleep to (hopefully) prevent re-entering the
    same countdown.

--- celestial/celestial.py
+++ celestial/celestial.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import asyncio
 import datetime
+import time
 class celestial:
     """BNS Celestial Basin Countdown Timer"""
 
@@ -48,13 +49,13 @@ class celestial:
             #But we don't want multiple messages
             flag = -1
 
-            #Keep going until timer <= 1 then break out
+            #Keep going until timer <= 2 then break out
             while True:
 
                 #Figure out current time
                 now = datetime.datetime.now()
 
-                #Figure out how many minutes and seconds are remaining
+                #Figure out how many minutes and seconds are remaining and current boss
                 cur_min = (BOSS_CD - ((now - known_spawn).seconds) % BOSS_CD) // 60
                 cur_sec = (BOSS_CD - ((now - known_spawn).seconds) % BOSS_CD) % 60
                 time_left = (cur_min * 60) + cur_sec
@@ -62,24 +63,28 @@ class celestial:
                 #If we have only 3 minutes remaining, send notification to server
                 if time_left <= 180:
                     if flag == -1:
-                        spawn_msg = await self.bot.say("@here " + boss[i] + " spawning in 3 minutes! (if order is wrong PM " + OWNER + ")")
+                        spawn_msg = await self.bot.say(" here " + boss[i] + " spawning in 3 minutes! (if order is wrong PM " + OWNER + ")")
                         flag = 0
 
                 #If the boss has spawned, then move boss index accordingly and delete notification message
                 #Timer sometimes jumps over 0s so we need some leeway
+                #If we enter early, make sure we don't leave early
                 if time_left <= 2:
                     await self.bot.delete_message(spawn_msg)
-                    if i == 2:
-                        i = 0
-                    elif i < 2:
-                        i += 1
-                    else:
-                        i = 3
+                    time.sleep(2)
                     break
 
                 #Have to subtract the seconds in our minutes from our total seconds
                 await self.bot.edit_message(countdown_msg, new_content=("```css" + "\n" + "[" + boss[i] + "] - [" + SERVER + "] " + "\nTimer: " + str(cur_min) + " minutes and " + str(cur_sec) + " seconds  ```"))
                 await asyncio.sleep(1)
+
+            #Move boss index accordingly
+            if i == 2:
+                i = 0
+            elif i == 3:
+                i = 3
+            else:
+                i += 1
 
 def setup(bot):
     bot.add_cog(celestial(bot))
